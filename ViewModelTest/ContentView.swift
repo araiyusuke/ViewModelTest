51755e6b64363d3e0ecb7cdf6b5f5daf234344ff
arai <info.yusuke.arai@gmail.com>
2022-08-21 21:52:41 +0900

    Initial Commit

---
+++ ViewModelTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewModelTest
+//
+//  Created by 名前なし on 2022/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewModelTest/ViewModelTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModelTestApp.swift
+//  ViewModelTest
+//
+//  Created by 名前なし on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewModelTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

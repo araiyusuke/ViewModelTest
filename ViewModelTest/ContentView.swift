56da5fb128dc050985b6d5099089498d669a881b
arai <info.yusuke.arai@gmail.com>
2022-08-25 14:48:02 +0900

    init

--- ViewModelTest/ContentView.swift
+++ ViewModelTest/ContentView.swift
@@ -5,33 +5,42 @@ import Combine
 
 extension ViewModel {
     enum Action {
-        case tapCreateAccountButton
         case onAppear
+        case onCommitInputName
+        case onInputComplete
     }
 }
 
 
 class ViewModel: ObservableObject {
-
-    @Published var count: Int = 0
+    
+    @Published var name: String = ""
+    @Published var password: String = ""
+    @Published var isLogin = false
     var cancellables = Set<AnyCancellable>()
     private let actionSubject = PassthroughSubject<Action, Never>()
-
+    
     init() {
         actionSubject
-              .sink(receiveValue: { actionType in
-                  switch actionType {
-                  case .tapCreateAccountButton:
-                      self.count += 1
-                  case .onAppear:
-                      print("onAppear")
-                  }
-              })
+            .sink(receiveValue: { actionType in
+                switch actionType {
+                case .onAppear:
+                    print("onAppear")
+                case .onCommitInputName:
+                    print(self.name)
+                case .onInputComplete:
+                    if self.name == "test" && self.password == "test" {
+                        self.isLogin = true
+                    } else {
+                        self.isLogin = false
+                    }
+                }
+            })
             .store(in: &cancellables)
     }
     func send(_ action: Action) {
-           actionSubject.send(action)
-       }
+        actionSubject.send(action)
+    }
 }
 
 extension ViewModel {
@@ -41,27 +50,68 @@ extension ViewModel {
 }
 
 struct ContentView: View {
-
+    
     @StateObject var viewModel: ViewModel
-
+    
     var body: some View {
-        ZStack {
-            Text(viewModel.count.description)
-                .padding()
-                .onTapGesture {
-                    viewModel.send(.tapCreateAccountButton)
-                    #if DEVELOP
-                        print("DEVELOP")
-                    #else
-                        print("OTHER")
-                    #endif
-                }
-
+        
+        Group {
+            if viewModel.isLogin {
+                memberInfo
+            } else {
+                loginForm
+                
+            }
         }
         .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-
         .onAppear {
-//            viewModel.send(.onAppear)
+            viewModel.send(.onAppear)
+        }
+    }
+    
+    var memberInfo: some View {
+        Text("ようこそ、\(viewModel.name)さん!")
+    }
+    
+    var loginForm: some View {
+        VStack {
+            
+            Spacer()
+            
+            TextField(
+                "あなたの名前",
+                text: $viewModel.name,
+                onEditingChanged: { begin in
+                },
+                onCommit: {
+                    viewModel.send(.onCommitInputName)
+                }
+            )
+            .onReceive(Just($viewModel.name)) { name in
+                print(name)
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())  // 入力域のまわりを枠で囲む
+            
+            SecureField("8〜16文字のパスワードを入力してください。", text: $viewModel.password, onCommit: {
+            })
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .keyboardType(.alphabet)
+            
+            Spacer()
+            
+            Button(action: {
+                viewModel.send(.onInputComplete)
+            }){
+                Text("ログイン")
+            }
+            Spacer()
+            
+            
+        }
+        .padding()
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        .onAppear {
+            viewModel.send(.onAppear)
         }
     }
 }
--- ViewModelTestTests/ViewModelTestTests.swift
+++ ViewModelTestTests/ViewModelTestTests.swift
@@ -13,12 +13,25 @@ import SnapshotTesting
 @testable import ViewModelTest
 
 class ViewModelTestTests: XCTestCase {
+
     let record = false
 
-    func testExample() throws {
+    func testExample1() throws {
         let viewModel = ViewModel()
         let contentView = ContentView(viewModel: viewModel)
-        viewModel.send(.tapCreateAccountButton)
+        viewModel.name = "test"
+        viewModel.password = "test"
+        viewModel.send(.onInputComplete)
+        sleep(4)
+        assertSnapshot(matching: contentView, as: .image, record: record)
+    }
+
+    func testExample2() throws {
+        let viewModel = ViewModel()
+        let contentView = ContentView(viewModel: viewModel)
+        viewModel.name = "魚さん"
+        viewModel.send(.onCommitInputName)
+
         assertSnapshot(matching: contentView, as: .image, record: record)
     }
 }
